e9178cb12cf5fdc480d592498b438a6cb3f576df
koungq <guu119@naver.com>
2024-07-08 23:19:35 +0900

    Feat: Day enum 리스트 관리를 위한 Converter

---
+++ src/main/java/cocone/wero/apro/domain/team/converter/DayListConverter.java
@@ -0,0 +1,36 @@
+package cocone.wero.apro.domain.team.converter;
+
+import cocone.wero.apro.domain.team.entity.enums.Day;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+
+import java.io.IOException;
+import java.util.List;
+
+public class DayListConverter implements AttributeConverter<List<Day>, String> {
+
+    private static final ObjectMapper mapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+
+    @Override
+    public String convertToDatabaseColumn(List<Day> attribute) {
+        try {
+            return mapper.writeValueAsString(attribute);
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Override
+    public List<Day> convertToEntityAttribute(String dbData) {
+        try {
+            return mapper.readValue(dbData, new TypeReference<>() {});
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}

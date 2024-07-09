f11defd90cbe6da06a79deb9a3458f5eb96163b4
koungq <guu119@naver.com>
2024-07-09 16:22:26 +0900

    Chore: 컨트롤러 스웨거 명시

--- src/main/java/cocone/wero/apro/domain/user/presentation/UserController.java
+++ src/main/java/cocone/wero/apro/domain/user/presentation/UserController.java
@@ -3,6 +3,7 @@ package cocone.wero.apro.domain.user.presentation;
 import cocone.wero.apro.domain.user.application.dto.UserDTO;
 import cocone.wero.apro.domain.user.application.usecase.UserUseCase;
 import cocone.wero.apro.global.common.response.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +15,13 @@ public class UserController {
 
     private final UserUseCase userUseCase;
 
+    @Operation(summary = "이메일 중복 검사", description = "회원가입 전 이메일(아이디) 중복 검사")
     @GetMapping("/email")
     public CommonResponse<Boolean> checkEmail(@RequestBody @Valid UserDTO.CheckEmail dto) {
         return CommonResponse.createSuccess(userUseCase.checkEmail(dto.email()));
     }
 
+    @Operation(summary = "회원 가입", description = "일반 유저 회원 가입")
     @PostMapping("/sign-up")
     public CommonResponse<Void> signUp(@RequestBody @Valid UserDTO.SignUp dto) {
         userUseCase.signUp(dto);

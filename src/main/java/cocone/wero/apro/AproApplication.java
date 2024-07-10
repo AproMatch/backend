2d41328357c529e0b421134e4c1c361221104616
koungq <guu119@naver.com>
2024-07-10 12:30:01 +0900

    Feat: CreatedAt, UpdatedAt 정보 자동 저장

--- src/main/java/cocone/wero/apro/AproApplication.java
+++ src/main/java/cocone/wero/apro/AproApplication.java
@@ -2,7 +2,9 @@ package cocone.wero.apro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class AproApplication {
 

a4c89a78808436fe549c26be3389f565ece1e8c3
김경규 <96408601+KoungQ@users.noreply.github.com>
2024-07-12 00:30:43 +0900

    @SpringBootTest 삭제
    Signed-off-by: 김경규 <96408601+KoungQ@users.noreply.github.com>

--- src/test/java/cocone/wero/apro/AproApplicationTests.java
+++ src/test/java/cocone/wero/apro/AproApplicationTests.java
@@ -1,9 +1,7 @@
 package cocone.wero.apro;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AproApplicationTests {
 
 	@Test
